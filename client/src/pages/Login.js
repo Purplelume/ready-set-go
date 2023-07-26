383b7354a588770cb6f8e0fcdc87b683b941fe45
Your Name <you@example.com>
2023-07-26 21:21:57 +1000

    Trying some desperate styling

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -43,8 +43,8 @@ const Login = (props) => {
     return (
         <main className="flex-row justify-center mb-4">
             <div className="col-12 col-lg-10">
-                <div className="card">
-                    <h4 className="card-header bg-dark text-light p-2">Login</h4>
+                <div className="auth-form-container">
+                    <h4 className="login-form">Login</h4>
                     <div className="card-body">
                         {data ? (
                             <p>
@@ -70,7 +70,7 @@ const Login = (props) => {
                                     onChange={handleChange}
                                 />
                                 <button
-                                    className="btn btn-block btn-info"
+                                    className="link-btn"
                                     style={{ cursor: 'pointer' }}
                                     type="submit"
                                 >
